84c742c61fd9883756f0f862838ede6b03b7fe15
Mitchell Daw <mitchelljdaw@gmail.com>
2018-03-08 19:05:30 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 $(document).ready(function(){
   $("#circle").click(function(){
-    $("#circle").animate({height: "300px", width: "300px", top: "30%", left: "35%"})
   })
   $("#square").click(function(){
-    $("#square").css({height: "300px", width: "300px", top: "30%", left: "35%"})
   })
-
   $("#triangle-up").click(function(){
-    $("#triangle-up").css({"animation-name": "move",
-    "animation-duration": ".6s",
-    "animation-iteration-count": "infinite",
-    "animation-direction": "alternate"})
-  })
 });
